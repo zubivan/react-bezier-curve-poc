607304ae6fd8b4326fb0d726955f582b2519af7a
Ivan Zub <zub.ivan@gmail.com>
2016-10-14 15:09:28 +0200

    Add function to calculate bounding box
    Stolen from https://github.com/kangax/fabric.js/blob/c34027cdfdbd1084a90cac4e74d6d2795dfa9025/src/util/arc.js

---
+++ src/components/utils.js
@@ -0,0 +1,81 @@
+/**
+ * Calculate bounding box of a beziercurve
+ * @param {Number} x0 starting point
+ * @param {Number} y0
+ * @param {Number} x1 first control point
+ * @param {Number} y1
+ * @param {Number} x2 secondo control point
+ * @param {Number} y2
+ * @param {Number} x3 end of beizer
+ * @param {Number} y3
+ */
+export function getBoundsOfCurve(x0, y0, x1, y1, x2, y2, x3, y3) {
+    const {sqrt, min, max, abs} = Math;
+
+    let tvalues = [];
+    let bounds = [[], []];
+    let a, b, c, t, t1, t2, b2ac, sqrtb2ac;
+
+    b = 6 * x0 - 12 * x1 + 6 * x2;
+    a = -3 * x0 + 9 * x1 - 9 * x2 + 3 * x3;
+    c = 3 * x1 - 3 * x0;
+
+    for (var i = 0; i < 2; ++i) {
+        if (i > 0) {
+            b = 6 * y0 - 12 * y1 + 6 * y2;
+            a = -3 * y0 + 9 * y1 - 9 * y2 + 3 * y3;
+            c = 3 * y1 - 3 * y0;
+        }
+
+        if (abs(a) < 1e-12) {
+            if (abs(b) < 1e-12) {
+                continue;
+            }
+            t = -c / b;
+            if (0 < t && t < 1) {
+                tvalues.push(t);
+            }
+            continue;
+        }
+        b2ac = b * b - 4 * c * a;
+        if (b2ac < 0) {
+            continue;
+        }
+        sqrtb2ac = sqrt(b2ac);
+        t1 = (-b + sqrtb2ac) / (2 * a);
+        if (0 < t1 && t1 < 1) {
+            tvalues.push(t1);
+        }
+        t2 = (-b - sqrtb2ac) / (2 * a);
+        if (0 < t2 && t2 < 1) {
+            tvalues.push(t2);
+        }
+    }
+
+    var x, y, j = tvalues.length, jlen = j, mt;
+    while (j--) {
+        t = tvalues[j];
+        mt = 1 - t;
+        x = (mt * mt * mt * x0) + (3 * mt * mt * t * x1) + (3 * mt * t * t * x2) + (t * t * t * x3);
+        bounds[0][j] = x;
+
+        y = (mt * mt * mt * y0) + (3 * mt * mt * t * y1) + (3 * mt * t * t * y2) + (t * t * t * y3);
+        bounds[1][j] = y;
+    }
+
+    bounds[0][jlen] = x0;
+    bounds[1][jlen] = y0;
+    bounds[0][jlen + 1] = x3;
+    bounds[1][jlen + 1] = y3;
+    var result = [
+        {
+            x: min.apply(null, bounds[0]),
+            y: min.apply(null, bounds[1])
+        },
+        {
+            x: max.apply(null, bounds[0]),
+            y: max.apply(null, bounds[1])
+        }
+    ];
+    return result;
+}

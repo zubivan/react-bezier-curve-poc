44b8ef75eef95c7401ca1afa7da6d2cf585d1eae
Ivan Zub <zub.ivan@gmail.com>
2016-10-14 15:12:01 +0200

    Add svg marker to display an arrow

--- src/components/bezier-curve.js
+++ src/components/bezier-curve.js
@@ -5,9 +5,16 @@ export const BezierCurve = ({origin, destination, controlPoint1, controlPoint2})
     return (
             <div style={{width: "100%", height: "100%"}}> 
                 <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+                  <defs>
+                        <marker id="markerArrow" markerWidth="13" markerHeight="13" refX="2" refY="6"
+                            orient="auto">
+                            <path d="M2,2 L2,11 L10,6 L2,2" fill="black" />
+                        </marker>
+                    </defs>
                     <path d={path} 
                         stroke="black"
-                        fill="transparent"
+                        fill="transparent" 
+                        markerEnd="url(#markerArrow)"
                         />
                 </svg>
             </div>

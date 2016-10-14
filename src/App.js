44d2827257f008e243e4b5fac6bd9ba92011fde2
Ivan Zub <zub.ivan@gmail.com>
2016-10-14 15:11:43 +0200

    Add an extra div to display a bounding box

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BezierCurve } from './components/bezier-curve';
+import { getBoundsOfCurve } from './components/utils';
 
 class App extends Component {
   state = {
@@ -12,9 +13,19 @@ class App extends Component {
   render() {
     const {origin, destination, controlPoint1, controlPoint2} = this.state;
 
+    var bounds = getBoundsOfCurve(origin.x, origin.y, controlPoint1.x, controlPoint1.y, controlPoint2.x, controlPoint2.y, destination.x, destination.y);
+
+    var style = {
+      left: (bounds[0].x),
+      top: (bounds[0].y),
+      width: (bounds[1].x - bounds[0].x),
+      height: (bounds[1].y - bounds[0].y)
+    };
+
     return (
       <div>
         <div style={{ width: 500, height: 500, position: "relative", border: "1px lightgray solid" }}>
+          <div style={{ border: "1px red solid", position: "absolute", ...style}}/>
           <BezierCurve origin={origin} destination={destination} controlPoint1={controlPoint1} controlPoint2={controlPoint2} />
         </div>
       </div>
